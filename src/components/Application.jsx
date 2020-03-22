0aba7c3c8fe4f63972e66baed0fe2efcaf2ff70d
Yarek <jrslwlkn@gmail.com>
2020-03-21 20:55:03 -0500

    state from firebase

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -5,46 +5,13 @@ import Posts from './Posts';
 
 class Application extends Component {
   state = {
-    posts: [
-      {
-        id: '1',
-        title: 'A Very Hot Take',
-        content:
-          'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!',
-        user: {
-          uid: '123',
-          displayName: 'Bill Murray',
-          email: 'billmurray@mailinator.com',
-          photoURL: 'https://www.fillmurray.com/300/300'
-        },
-        stars: 1,
-        comments: 47
-      },
-      {
-        id: '2',
-        title: 'The Sauciest of Opinions',
-        content:
-          'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis suscipit repellendus modi unde cumque, fugit in ad necessitatibus eos sed quasi et! Commodi repudiandae tempora ipsum fugiat. Quam, officia excepturi!',
-        user: {
-          uid: '456',
-          displayName: 'Mill Burray',
-          email: 'notbillmurray@mailinator.com',
-          photoURL: 'https://www.fillmurray.com/400/400'
-        },
-        stars: 3,
-        comments: 0
-      }
-    ]
+    posts: []
   };
 
   componentDidMount = async () => {
     const snapshot = await firestore.collection('posts').get();
-    snapshot.forEach(doc => {
-      const { id } = doc;
-      const data = doc.data();
-      console.log({ id, data });
-    });
-    console.log({ snapshot });
+    const posts = snapshot.docs.map(d => ({ id: d.id, ...d.data() }));
+    this.setState({ posts });
   };
 
   handleCreate = post => {
